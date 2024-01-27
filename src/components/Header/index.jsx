73e6b0ef619bab611e777048b7b5daf4d352a305
Santiago Hanine <santihanine@gmail.com>
2024-01-27 20:59:51 -0300

    Refactor Header component to add scroll functionality

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,50 +1,68 @@
-import { useState, useEffect } from 'react'
-import {FiMenu} from 'react-icons/fi'
-import { HeaderItems } from './HeaderItems'
-
+import { useState, useEffect } from 'react';
+import { FiMenu } from 'react-icons/fi';
+import { HeaderItems } from './HeaderItems';
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(true);
+  let [previousScrollPosition, setPreviousScrollPosition] = useState(0);
+  let [currentScrollPosition, setCurrentScrollPosition] = useState(0);
 
-    const [isOpen, setIsOpen] = useState(false)
-    const [scrolled, setScrolled] = useState(false);
 
+  useEffect(() => {
 
-    useEffect(() => {
-      const onScroll = () => {
-        if (window.scrollY > 62) {
-          setScrolled(true);
-        } else {
-          setScrolled(false);
-        }
+    window.addEventListener('scroll', function (e) {
+      setCurrentScrollPosition(window.scrollY)
+      //Subtract the two and conclude
+      if (previousScrollPosition - currentScrollPosition < 0) {
+        setScrolled(false);
+      } else if (previousScrollPosition - currentScrollPosition > 0) {
+        setScrolled(true);
       }
-  
-      window.addEventListener("scroll", onScroll);
-  
-      return () => window.removeEventListener("scroll", onScroll);
-    }, [])
 
+      // Update the previous value
+      setPreviousScrollPosition(currentScrollPosition)
+    });
+  }, [currentScrollPosition]);
 
   return (
-      <div className={`lg:hidden w-screen fixed top-0 z-10 transition-[all_5s] h-20 ${scrolled ? 'bg-white' : 'bg-gray-500'}`}>
-      <h2 className={`${scrolled ? 'text-black' : 'text-white'} text-xl tracking-wider font-kanit absolute left-9 top-7`}>Santiago Hanine</h2>
-        {!isOpen &&
-            <FiMenu className={`${scrolled ? 'text-black' : 'text-white'} absolute right-8 top-4 h-12 w-12`} onClick={() => setIsOpen(!isOpen)}/>
-        }
-    
-        {isOpen && (
-        <div className='bg-white'>
-            <FiMenu onClick={()=> setIsOpen(!isOpen)} className={`${scrolled ? 'text-black' : 'text-white'}  absolute right-8 z-10 top-4 h-12 w-12`}/>
-            
-            <div className={`absolute top-[64px] mt-4 right-0 pr-10 p-2 w-full  ${isOpen ? 'block' : 'hidden' } text-right ${scrolled ? 'bg-white' : 'bg-gray-500'}  z-10 animate-[menuAppear_0.4s_ease-in-out]`}>
-                {HeaderItems().map(item => (
-                    <div className='w-full' key={item.name}>
-                        <a className={`text-black ${scrolled ? 'text-black': 'text-white'} font-kanit text-2xl flex flex-col my-5 text-right`} href={item.link}>{item.name}</a>
-                    </div>
-                ))}
-            </div>
-        </div>
-        )}
-    </div>
+    <div
+      className={`lg:hidden w-screen fixed top-0 z-10 h-20 transition-all duration-500 transform ${
+        scrolled ? 'bg-gray-500 animate-menuAppear' : 'animate-menuDisappear'
+      }`}
+    >
+      <h2
+      className={`${scrolled ? '' : 'hidden'} text-xl tracking-wider font-kanit absolute left-9 top-7`}
+      >
+        Santiago Hanine
+      </h2>
+      <FiMenu
+        className={`${scrolled ? '' : 'hidden'} text-black absolute right-8 top-4 h-12 w-12 z-50`}
+        onClick={() => setIsOpen(!isOpen)}
+      />
 
-  )
-}
+      {isOpen && (
+        <div className="bg-gray-500">
+
+          <div
+            className={`absolute top-[40px] mt-4 right-0 pr-10 p-2 w-full ${
+              isOpen ? 'block' : 'hidden'
+            } text-right bg-gray-500 z-10`}
+          >
+            {HeaderItems().map((item) => (
+              <div className="w-full" key={item.name}>
+                <a
+                  onClick={() => setIsOpen(!isOpen)}
+                  className={`text-black ${scrolled ? 'text-black' : 'text-white'} font-kanit text-2xl flex flex-col my-5 text-right`}
+                  href={item.link}
+                >
+                  {item.name}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
