1739385a003fcc9c8de5b38bb09d3cff0ab9cf2e
Santiago Hanine <santihanine@gmail.com>
2024-01-27 01:06:36 -0300

    Add Experience component and list data

---
+++ src/components/Experience/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import list from './list'
+
+export const Experience = () => {
+  return (
+    <div className="flex flex-col items-center max-w-[80%] lg:max-w-[60%] lg:pl-28 justify-center mx-auto py-20">
+
+        <h3 className='mb-24 relative text-center md:text-6xl text-5xl tracking-wider text-white font-kanit uppercase '>
+            My Experience
+        </h3>
+            <ol class="relative border-s border-gray-200 ml-3"> 
+            
+            {list().map(item => (
+                <li class="mb-10 ms-4"> 
+            
+            <div class="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-strong-blue bg-strong-blue">
+                </div> 
+                <time class="mb-1 text-sm font-normal leading-none text-white/80">{item.date}</time> 
+                <h3 class="text-lg font-semibold text-strong-blue mt-2"> 
+                    {item.position}
+                </h3> 
+                <p class="mb-4 text-base font-normal text-white "> 
+                    {item.description}
+                </p> 
+             </li>
+            ))}
+</ol>
+    </div>
+  )
+}
+
+export default Experience
---
+++ src/components/Experience/list.jsx
@@ -0,0 +1,19 @@
+
+
+const list = () => {
+    const listExperience = [
+        {
+            position: 'Web Developer at 40Q',
+            description: 'I worked as a web developer at 40Q, an agency that provides web development services to clients from every part of the world.',
+            date: 'March 2023 - Present'
+        },
+        {
+            position: 'Freelance Frontend Developer',
+            description: 'I have worked with clients as a freelancer, helping them to build their websites and web applications.',
+            date: 'December 2022'
+        },
+    ]
+  return listExperience
+}
+
+export default list
